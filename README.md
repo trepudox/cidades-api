df2a75cbf16b5380e194246553baa9d2c57d5563
Marco Aurélio Queiroz <teoaa2@gmail.com>
2021-05-12 23:29:37 -0300

    commit inicial

---
+++ README.md
@@ -0,0 +1,3 @@
+# API de consulta de cidades
+
+API desenvolvida durante bootcamp da everis na DIO.
---
+++ cidades-api/src/test/java/com/trepudox/cidadesapi/CidadesApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.trepudox.cidadesapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CidadesApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

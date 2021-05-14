ceb8f067d51217011ebea3318873024ddf3149f5
Marco Aurélio Queiroz <teoaa2@gmail.com>
2021-05-14 01:50:48 -0300

    método de busca por nome e distância entre uma cidade e outra

--- cidades-api/src/main/java/com/trepudox/cidadesapi/repository/CidadeRepository.java
+++ cidades-api/src/main/java/com/trepudox/cidadesapi/repository/CidadeRepository.java
@@ -2,8 +2,16 @@ package com.trepudox.cidadesapi.repository;
 
 import com.trepudox.cidadesapi.model.Cidade;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface CidadeRepository extends JpaRepository<Cidade, Long> {
+
+    public List<Cidade> findAllByNomeContainingIgnoreCase(String nome);
+
+    @Query(value = "SELECT earth_distance(ll_to_earth(?1, ?2), ll_to_earth(?3, ?4))", nativeQuery = true)
+    public Double distanciaEntre(Double lat1, Double lon1, Double lat2, Double lon2);
 }

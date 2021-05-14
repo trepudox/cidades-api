bed4ef38f8188add596e442097cd12db5eb17bf8
Marco Aurélio Queiroz <teoaa2@gmail.com>
2021-05-14 01:47:35 -0300

    criando o tipo de dado Ponto

--- cidades-api/src/main/java/com/trepudox/cidadesapi/model/Cidade.java
+++ cidades-api/src/main/java/com/trepudox/cidadesapi/model/Cidade.java
@@ -1,15 +1,20 @@
 package com.trepudox.cidadesapi.model;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.trepudox.cidadesapi.util.PointType;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import org.hibernate.annotations.Type;
+import org.hibernate.annotations.TypeDef;
+import org.springframework.data.geo.Point;
 
 import javax.persistence.*;
 
 @Entity
 @Table(name = "cidade")
+@TypeDef(name = "point", typeClass = PointType.class)
 @Getter @Setter @NoArgsConstructor @AllArgsConstructor
 public class Cidade {
 
@@ -25,8 +30,9 @@ public class Cidade {
 
     private Integer ibge;
 
+    @Type(type = "point")
     @Column(name = "lat_lon")
-    private String latLon;
+    private Point latLon;
 
     private Double latitude;
 

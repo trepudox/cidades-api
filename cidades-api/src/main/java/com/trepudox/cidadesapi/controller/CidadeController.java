b01a3ed4e1a1fec7732ff696c02a671af299db7c
Marco Aurélio Queiroz <teoaa2@gmail.com>
2021-05-14 01:51:32 -0300

    padronização de métodos e criação de endpoint de busca por nome

--- cidades-api/src/main/java/com/trepudox/cidadesapi/controller/CidadeController.java
+++ cidades-api/src/main/java/com/trepudox/cidadesapi/controller/CidadeController.java
@@ -1,17 +1,15 @@
 package com.trepudox.cidadesapi.controller;
 
 import com.trepudox.cidadesapi.model.Cidade;
-import com.trepudox.cidadesapi.repository.CidadeRepository;
+import com.trepudox.cidadesapi.service.CidadeService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
 import java.util.Optional;
 
 @RestController
@@ -19,18 +17,28 @@ import java.util.Optional;
 public class CidadeController {
 
     @Autowired
-    private CidadeRepository repository;
+    private CidadeService service;
 
     @GetMapping
     public ResponseEntity<Page<Cidade>> getAllPageable(Pageable page) {
-        return ResponseEntity.status(HttpStatus.OK).body(repository.findAll(page));
+        return ResponseEntity.status(HttpStatus.OK).body(service.findAll(page));
     }
 
     @GetMapping("/{id}")
     public ResponseEntity<Cidade> getById(@PathVariable Long id) {
-        Optional<Cidade> c = repository.findById(id);
+        Optional<Cidade> c = service.findById(id);
 
         return c.isPresent() ? ResponseEntity.status(HttpStatus.OK).body(c.get()) :
                 ResponseEntity.status(HttpStatus.NOT_FOUND).build();
+    }
+
+    @GetMapping("/containing")
+    public ResponseEntity<List<Cidade>> getAllByNomeContainingIgnoreCase(@RequestParam(name = "nome") String nome) {
+        List<Cidade> lista = service.findAllByNomeContainingIgnoreCase(nome);
+
+        if (lista.isEmpty())
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
+
+        return ResponseEntity.status(HttpStatus.OK).body(lista);
     }
 }
